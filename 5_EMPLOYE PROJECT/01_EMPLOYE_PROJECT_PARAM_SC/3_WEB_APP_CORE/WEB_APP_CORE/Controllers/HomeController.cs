7eb89f5f24f83bf27ebc65898109066dbc937c43
Thillaishanmugam1998 <shansenthil72@gmail.com>
2024-09-03 17:14:40 +0530

    Added Connection string

---
+++ 5_EMPLOYE PROJECT/01_EMPLOYE_PROJECT_PARAM_SC/3_WEB_APP_CORE/WEB_APP_CORE/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using WEB_APP_CORE.Models;
+using Microsoft.AspNetCore.Mvc;
+using WEB_APP_CORE.CallWebAPI;
+using Newtonsoft.Json;
+
+namespace WEB_APP_CORE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public JsonResult GetEmployeeById(int id)
+        {
+            string baseUrl      = "http://localhost:15738";
+            string controller   = "Employee";
+            string method       = "GetEmployeeById";
+            string queryParam   = $"id={id}";
+
+            string response     = WebApiClient.Get(baseUrl, controller, method, queryParam);
+            //var employee      = JsonConvert.DeserializeObject<Employee>(response);
+
+            return Json(response);
+        }
+    }
+
+}
