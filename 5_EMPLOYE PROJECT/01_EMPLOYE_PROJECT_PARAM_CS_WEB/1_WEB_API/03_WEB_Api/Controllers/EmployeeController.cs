7b3315b08693c6d6aad15c1f87161268efbf5b14
Thillaishanmugam1998 <shansenthil72@gmail.com>
2024-09-04 16:45:18 +0530

    Added WEB Project

---
+++ 5_EMPLOYE PROJECT/01_EMPLOYE_PROJECT_PARAM_CS_WEB/1_WEB_API/03_WEB_Api/Controllers/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BAL;
+
+namespace WEBApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        #region 01. INITIALIZE BAL:-
+        private readonly EmployeeBAL _employeeBAL = new EmployeeBAL();
+        #endregion
+
+        #region 02. INITIALIZE METHOD:-
+        [HttpGet]
+        public string  Get()
+        {
+            return "Welcome to API";
+        }
+        #endregion
+
+        #region 03. INSERT EMPLOYEE:-
+        [HttpGet("InsertEmployee")]
+        public int InsertEmployee(string name, string email, string mobile, string city)
+        {
+            int result = _employeeBAL.InsertEmployee(name, email, mobile, city);
+            return (result);
+        }
+        #endregion
+
+        #region 04. UPDATE EMPLOYEE:-
+        [HttpGet("UpdateEmployee")]
+        public int UpdateEmployee(int id, string name, string email, string mobile, string city)
+        {
+            int result = _employeeBAL.UpdateEmployee(id, name, email, mobile, city);
+            return (result);
+        }
+        #endregion
+
+        #region 05. DELETE EMPLOYEE:-
+        [HttpGet("DeleteEmployee")]
+        public int DeleteEmployee(int id)
+        {
+            int result = _employeeBAL.SoftDeleteEmployee(id);
+            return result;
+
+        }
+        #endregion
+
+        #region 06. GET EMPLOYEE BY ID:-
+        [HttpGet("GetEmployeeById")]
+        public string GetEmployeeById(int id)
+        {
+            string result = _employeeBAL.GetEmployeeById(id);
+            return (result);
+        }
+        #endregion
+
+        #region 07. GET ALL EMPLOYEE:-
+        [HttpGet("GetAllEmployees")]
+        public string GetAllEmployees()
+        {
+            string result = _employeeBAL.GetAllEmployees();
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ 5_EMPLOYE PROJECT/01_EMPLOYE_PROJECT_PARAM_CS_WEB/1_WEB_API/03_WEB_Api/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string City { get; set; }
+    }
+
+}
