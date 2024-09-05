3e4be7c80a886460f94cb1522645936d125aba90
Thillaishanmugam1998 <shansenthil72@gmail.com>
2024-09-05 14:18:12 +0530

    Added web

---
+++ 5_EMPLOYE PROJECT/SamplewebMVC/SamplewebMVC/Controllers/DashboardController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SamplewebMVC.BusinessLogicLayer;
+
+namespace SamplewebMVC.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly BAL _BAL;
+
+        #region 00. PAGES:-
+        // GET:  /Home/Dashboard
+        #endregion
+
+        #region 01. CONSTRUCTOR:-
+        public DashboardController()
+        {
+            _BAL = new BAL();
+        }
+        #endregion
+
+        #region 02. DASHBOARD PAGE:-
+        /// <summary>
+        /// Displays the dashboard page after a successful login.
+        /// </summary>
+        /// <returns>An IActionResult representing the dashboard view.</returns>
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        #endregion
+
+        public void InsertData()
+        {
+
+        }
+
+        public void UpdateData()
+        {
+        }
+
+
+        public void DeleteData()
+        {
+        }
+
+        public void GetallData()
+        {
+
+        }
+
+
+    }
+}
---
+++ 5_EMPLOYE PROJECT/SamplewebMVC/SamplewebMVC/Query/1_tbl_usercredentials.sql
@@ -0,0 +1,17 @@
+﻿--Create a table
+CREATE TABLE tbl_usercredentials (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    isdeleted BOOLEAN DEFAULT FALSE
+);
+
+--Insert a table
+INSERT INTO tbl_usercredentials (username, password)
+VALUES ('admin', '12345');
+
+--Select a table
+SELECT * FROM  tbl_usercredentials;
+
+--Drop a table
+DROP TABLE tbl_usercredentials;
